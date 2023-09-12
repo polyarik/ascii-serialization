e9d973c9be41abdc878849246bef81604f36710e
PolYarik <polyarik@pm.me>
2023-09-12 22:33:36 +0700

    ✨ add array deserialization from ASCII compression

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { serialize, deserialize } from "./seriliazation";
+import { simpleSerialize, simpleDeserialize } from "./simple-seriliazation";
+
+const arr = [1, 1, 1, 2, 1, 300, 298, 15, 127, 29, 29, 65, 29, 29, 29, 17];
+console.log(`initial:${arr}:`);
+
+const str = serialize(arr);
+console.log(`compressed:${str}:`);
+
+const result = deserialize(str);
+console.log(`decompressed:${arr}:`);
--- src/seriliazation.ts
+++ src/seriliazation.ts
@@ -1,8 +1,8 @@
 const MAX_LENGTH = 1000;
 const RANGE = 300;
 
-const bitsForCounts = BigInt(Math.ceil(Math.log2(MAX_LENGTH)));
-const bitsForNums = BigInt(Math.ceil(Math.log2(RANGE)));
+const bitsForCounts = Math.ceil(Math.log2(MAX_LENGTH));
+const bitsForNums = Math.ceil(Math.log2(RANGE));
 
 export function serialize(nums: number[]) {
   if (nums.length > MAX_LENGTH)
@@ -14,15 +14,42 @@ export function serialize(nums: number[]) {
   }, new Map<number, number>());
 
   let bits = 0n;
-  let uniqueNumsCount = 0n;
+  let uniqueCount = 0;
 
   occurrences.forEach((count, num) => {
-    const currBits = (BigInt(count) << bitsForNums) | BigInt(num);
-    bits |= currBits << BigInt(uniqueNumsCount * (bitsForCounts + bitsForNums));
-    uniqueNumsCount++;
+    const currBits = (BigInt(count) << BigInt(bitsForNums)) | BigInt(num);
+    bits |= currBits << BigInt(uniqueCount * (bitsForCounts + bitsForNums));
+    uniqueCount++;
   });
 
   return convertToASCII(bits);
+}
+
+export function deserialize(base128: string) {
+  const arr: number[] = [];
+  let bits = convertFromASCII(base128);
+
+  while (bits) {
+    let num = 0;
+
+    for (let i = 0; i < bitsForNums; i++) {
+      num += Number(bits & BigInt(1 << i));
+    }
+
+    let count = 0;
+
+    for (let i = 0; i < bitsForCounts; i++) {
+      if (bits & BigInt(1 << (i + bitsForNums))) count += Math.pow(2, i);
+    }
+
+    for (let i = 0; i < count; i++) {
+      arr.push(num);
+    }
+
+    bits >>= BigInt(bitsForCounts + bitsForNums);
+  }
+
+  return arr;
 }
 
 function convertToASCII(num: bigint) {
@@ -37,10 +64,13 @@ function convertToASCII(num: bigint) {
   return base128;
 }
 
-export function deserialize(str: string) {
-  //return number[]
-}
-
 function convertFromASCII(base128: string) {
-  //return BigNumber
+  let num = 0n;
+
+  for (let i = 0; i < base128.length; i++) {
+    const digit = base128.charCodeAt(i) * Math.pow(128, base128.length - 1 - i);
+    num += BigInt(digit);
+  }
+
+  return num;
 }
